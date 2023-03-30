e064dfe42d460eac13e5cb5026d03d439df0ce04
Joshua Francis <joshuafrancis19@gmail.com>
2023-03-30 23:06:25 +0100

    woring

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,18 +8,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\BigSur;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
-    #[Route('/u/user', name: 'app_user')]
-    public function createUser(ManagerRegistry $doctrine): Response
+    #[Route('/u/user/{uid}/', name: 'app_user')]
+    public function show(ManagerRegistry $doctrine, string $uid): Response
+    {
+        $user = $doctrine->getRepository(BigSur::class)->find($uid);
+
+        if(!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$uid
+            );
+        }
+
+        return new Response('Check out this great user: '.$user->getPid());
+    }
+
+    public function createUser(ManagerRegistry $doctrine, ValidatorInterface $validator): Response
     {
         $entityManager = $doctrine->getManager();
 
         $user = new BigSur();
         $user->setUid('BlahBlah1234');
-        $user->setPid('thePostID-1234');
-        $user->setAccess(1);
+        $user->setPid('justMyPostMap');
+        $user->setAccess('1');
+
+        $errors = $validator->validate($user);
+        if(count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
 
         $entityManager->persist($user);
 
